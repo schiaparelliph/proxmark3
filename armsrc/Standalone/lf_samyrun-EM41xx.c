bee05fc190998b1b86723b181cf5755500c27c83
schiaparelliph <61218209+schiaparelliph@users.noreply.github.com>
2020-04-25 14:39:26 +0800

    Update and rename lf_samyrun.c to lf_samyrun-EM41xx.c

--- armsrc/Standalone/lf_samyrun-EM41xx.c
+++ armsrc/Standalone/lf_samyrun-EM41xx.c
@@ -19,8 +19,10 @@
 
 #define OPTS 2
 
+uint64_t low[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
 void ModInfo(void) {
-    DbpString("  LF HID26 standalone - aka SamyRun (Samy Kamkar)");
+    DbpString("  LF EM4xx standalone - aka SamyRun (Samy Kamkar)");
 }
 
 // samy's sniff and repeat routine for LF
@@ -31,10 +33,51 @@ void ModInfo(void) {
 //  C = playing bank A
 //  D = playing bank B
 
+//J added below from lf_em410
+
+
+uint64_t ReversQuads(uint64_t bits) {
+    uint64_t result = 0;
+    for (int i = 0; i < 16; i++) {
+        result += ((bits >> (60 - 4 * i)) & 0xf) << (4 * i);
+    }
+    return result >> 24;
+}
+
+void FillBuff(uint8_t bit) {
+    memset(bba + buflen, bit, CLOCK / 2);
+    buflen += (CLOCK / 2);
+    memset(bba + buflen, bit ^ 1, CLOCK / 2);
+    buflen += (CLOCK / 2);
+}
+
+void ConstructEM410xEmulBuf(uint64_t id) {
+
+    int i, j, binary[4], parity[4];
+    buflen = 0;
+    for (i = 0; i < 9; i++)
+        FillBuff(1);
+    parity[0] = parity[1] = parity[2] = parity[3] = 0;
+    for (i = 0; i < 10; i++) {
+        for (j = 3; j >= 0; j--, id /= 2)
+            binary[j] = id % 2;
+        for (j = 0; j < 4; j++)
+            FillBuff(binary[j]);
+        FillBuff(binary[0] ^ binary[1] ^ binary[2] ^ binary[3]);
+        for (j = 0; j < 4; j++)
+            parity[j] ^= binary[j];
+    }
+    for (j = 0; j < 4; j++)
+        FillBuff(parity[j]);
+    FillBuff(0);
+}
+//J added above from lf_em
+
+
 void RunMod() {
     StandAloneMode();
     FpgaDownloadAndGo(FPGA_BITSTREAM_LF);
-    Dbprintf(">>  LF HID Read/Clone/Sim a.k.a SamyRun Started  <<");
+    Dbprintf(">>  LF EM41xx Read/Clone/Sim a.k.a modifiedSamyRun Started  <<");
 
     uint32_t high[OPTS], low[OPTS];
     int selected = 0;
@@ -82,7 +125,7 @@ void RunMod() {
 
             // findone, high, low, no ledcontrol (A)
             uint32_t hi = 0, lo = 0;
-            CmdHIDdemodFSK(1, &hi, &lo, 0);
+            CmdEM410xdemodFSK(1, &hi, &lo, 0);
             high[selected] = hi;
             low[selected] = lo;
 
@@ -108,8 +151,14 @@ void RunMod() {
             Dbprintf("[=] simulating %x | %x%08x", selected, high[selected], low[selected]);
 
             // high, low, no led control(A)  no time limit
-            CmdHIDsimTAGEx(0, high[selected], low[selected], 0, false, -1);
-
+            //J samyrun CmdHIDsimTAGEx(0, high[selected], low[selected], 0, false, -1);
+            //J below added from lf_em410
+            ConstructEM410xEmulBuf(ReversQuads(low[selected]));
+            FlashLEDs(100, 5);
+            SimulateTagLowFrequency(buflen, 0, 1);
+            //J above added from lf_em410
+            
+            
             DbpString("[=] simulating done");
 
             uint8_t leds = ((selected == 0) ? LED_A : LED_B) | LED_C;
@@ -126,7 +175,10 @@ void RunMod() {
             Dbprintf("[=]    cloning %x | %x%08x", selected, high[selected], low[selected]);
 
             // high2, high, low,  no longFMT
-            CopyHIDtoT55x7(0, high[selected], low[selected], 0);
+           //J samyrun CopyHIDtoT55x7(0, high[selected], low[selected], 0);
+            
+            //J
+            WriteEM410x(0, high[selected], low[selected], 0);
 
             DbpString("[=] cloned done");
 
