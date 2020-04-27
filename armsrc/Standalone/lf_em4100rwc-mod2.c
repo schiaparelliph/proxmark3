7ba301bcba616de53698e08e5c96743bcca2b60f
schiaparelliph <61218209+schiaparelliph@users.noreply.github.com>
2020-04-27 16:14:01 +0800

    Update lf_em4100rwc-mod2.c

--- armsrc/Standalone/lf_em4100rwc-mod2.c
+++ armsrc/Standalone/lf_em4100rwc-mod2.c
@@ -164,6 +164,7 @@ void RunMod() {
                 // Read mode.
                 LED_D_ON();
                 DbpString("State=1 read mode- click to read- hold to simulate");
+                button_pressed = -1;
                 if (button_pressed > 0) {
                     // Long press - switch to read mode
                     DbpString("Long Press- switch to state=2 simulate");
@@ -177,16 +178,17 @@ void RunMod() {
                                       
                     CmdEM410xdemod(1, &high[selected], &low[selected], 0);
                     
-                    Dbprintf("read card: %x",low[selected]);
+                    Dbprintf("read card: %x",high[selected]);
                     FlashLEDs(100, 5);
                     state = 1;
                 }
                 continue;
         } else if (state == 2) {
                 // Simulate mode
-                LED_C_ON();
-                LED_D_ON();
+                
+                
                 DbpString("State=2 simulate mode - click to simulate - hold to write");
+                button_pressed = -1;
                 if (button_pressed > 0) {
                     // Long press - switch to read mode
                     DbpString("Long Press - switch to state=3 write mode");
@@ -201,6 +203,8 @@ void RunMod() {
                     LED_Slot(selected);
                     ConstructEM410xEmulBuf(ReversQuads(low[selected]));
                     FlashLEDs(100, 5);
+                    LED_C_ON();
+                    LED_D_ON();
                     SimulateTagLowFrequency(buflen, 0, 1);
                     LED_Slot(selected);
                     state = 2; // Switch to simulate mode
